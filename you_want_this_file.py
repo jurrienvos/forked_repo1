16324031f1c9370c8b70936b227f3d897159dd70
Jurrien Vos <Jurrien.Vos@ses.com>
2024-10-15 14:54:57 +0200

    code blocks added

--- you_want_this_file.py
+++ you_want_this_file.py
@@ -7,3 +7,38 @@ def add(a,b):
     return a+b
 result=add(2,3)
 print("result =",result)
+
+
+age=2
+if age<0:
+    raise ValueError("no possible")
+
+
+age = -1
+try:
+    if age < 0:
+        raise ValueError("no possible")
+except ValueError as e:
+    print(f"An error occurred: {e}")
+
+
+try:
+    result = 10 / 0
+except ZeroDivisionError:
+    print("You can't divide by zero!")
+
+try:
+    file = open("data.txt", "r")
+except FileNotFoundError:
+    print("File not found!")
+finally:
+    print("Execution not completed.")
+
+
+class CustomError(Exception):
+    pass
+try:
+    raise CustomError("This is a custom error!")
+except CustomError as e:
+    print(e)
+
