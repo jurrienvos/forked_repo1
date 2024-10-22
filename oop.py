e9b31c0a90bbac7dcedaa222c9afaaa6b3f56e9c
Jurrien Vos <Jurrien.Vos@ses.com>
2024-10-22 17:13:10 +0200

    futrher work on oop.py

--- oop.py
+++ oop.py
@@ -1,7 +1,7 @@
 #######################################################
 ###################### OOP ############################
 
-#1. What is a Class?
+####1. What is a Class?
 class Dog:
     def bark(self): #self is required for instance methods in Python. It allows the method to access the instance's attributes and other methods.
         return "Woof!"
@@ -30,25 +30,46 @@ print(f'nose: {my_horse.nose()}')
 print(f'tail: {my_horse.tail()}')
 
 
+####3. Attributes
+class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+my_dog = Dog("Buddy", 5)
+print(my_dog.name) # Output: Buddy
 
-# #3. Attributes
-# class Dog:
-#     def __init__(self, name, age):
-#         self.name = name
-#         self.age = age
-# my_dog = Dog("Buddy", 5)
-# print(my_dog.name) # Output: Buddy
-#
-# #4. Methods
-# class Dog:
-#     def __init__(self, name, age):
-#         self.name = name
-#         self.age = age
-#     def bark(self):
-#         return f"{self.name} says Woof!"
-# my_dog = Dog("Buddy", 5)
-# print(my_dog.bark()) # Output: Buddy says Woof!
-#
+############### my code
+class mens:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+my_mens = mens("Buddy", 5)
+print(f'mens: {my_mens.name} is {my_mens.age}')
+
+
+####4. Methods
+class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def bark(self):
+        return f"{self.name} says Woof!"
+my_dog = Dog("Buddy", 5)
+print(my_dog.bark()) # Output: Buddy says Woof!
+
+############### my code
+class mens:
+    def __init__(self, name, age, length):
+        self.name = name
+        self.age = age
+        self.length=length
+    def human_traits(self):
+        return f"{self.name} is {self.age} yrs old and has length {self.length} cm"
+my_mens = mens("Buddy",5,160)
+print(my_mens.human_traits())
+
+
+
 # #5. Inheritance
 # class Animal:
 #     def speak(self):
@@ -58,3 +79,11 @@ print(f'tail: {my_horse.tail()}')
 #         return "Woof!"
 # my_dog = Dog()
 # print(my_dog.speak()) # Output: Animal speaks
+
+
+
+
+
+
+#############################################################################################################
+###################### https://www.w3resource.com/python-exercises/oop/index.php ############################
