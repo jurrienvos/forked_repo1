58208ae4206cac4f65bcd0806a9e45f2a131c90e
Jurrien Vos <Jurrien.Vos@ses.com>
2024-10-22 16:39:32 +0200

    added oop.py

---
+++ oop.py
@@ -0,0 +1,60 @@
+#######################################################
+###################### OOP ############################
+
+#1. What is a Class?
+class Dog:
+    def bark(self): #self is required for instance methods in Python. It allows the method to access the instance's attributes and other methods.
+        return "Woof!"
+#2. Creating an Object
+my_dog = Dog()
+print(my_dog.bark()) # Output: Woof!
+#if you'd remove self > TypeError: Dog.bark() takes 0 positional arguments but 1 was given.
+# Why? when you call my_dog.bark(), Python automatically passes the instance my_dog to the bark method as the first argument.
+#But since you've removed self from the method definition, Python expects bark() to take no arguments, hence the error.
+
+############### my code
+def bark():
+    return "Woof!"
+print(f'code written by me {bark()}')
+
+class horse:
+    def legs(self):
+        return "4 legs"
+    def nose(self):
+        return "wet nose"
+    def tail(self):
+        return "long tail"
+my_horse=horse()
+print(f'legs: {my_horse.legs()}')
+print(f'nose: {my_horse.nose()}')
+print(f'tail: {my_horse.tail()}')
+
+
+
+# #3. Attributes
+# class Dog:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+# my_dog = Dog("Buddy", 5)
+# print(my_dog.name) # Output: Buddy
+#
+# #4. Methods
+# class Dog:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#     def bark(self):
+#         return f"{self.name} says Woof!"
+# my_dog = Dog("Buddy", 5)
+# print(my_dog.bark()) # Output: Buddy says Woof!
+#
+# #5. Inheritance
+# class Animal:
+#     def speak(self):
+#         return "Animal speaks"
+# class Dog(Animal):
+#     def bark(self):
+#         return "Woof!"
+# my_dog = Dog()
+# print(my_dog.speak()) # Output: Animal speaks
